8dfaa2eb51bb28a99e5dc8b9b9153145ba1d926f
mhdr1zky <89838101+mhdr1zky@users.noreply.github.com>
2022-01-18 04:08:38 -0800

    add demo picture

--- core/create_generate.php
+++ core/create_generate.php
@@ -10,7 +10,7 @@ $string .= "
   <div class=\"modal-dialog\" role=\"document\">
     <div class=\"modal-content\">
       <div class=\"modal-header\">
-        <h5 class=\"modal-title\" id=\"modal_createLabel\">Tambah Data</h5>
+        <h5 class=\"modal-title\" id=\"modal_createLabel\">Create Data</h5>
         <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">
           <span aria-hidden=\"true\">&times;</span>
         </button>
